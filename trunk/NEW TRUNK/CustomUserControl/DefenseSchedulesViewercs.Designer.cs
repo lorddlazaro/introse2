b97ca4fb427484a0b3b951726850a92c558ef2fc
jules_macatangay@dlsu.ph <jules_macatangay@dlsu.ph@8da0477a-1d21-c455-1106-59f872cc08d5>
2013-03-31 07:44:59 +0000

    Commiting 'cause technically it's done.
    Probably ang only bug i can think of is that it doesn't automatically refresh/update if you schedule new defenses

--- trunk/NEW TRUNK/CustomUserControl/DefenseSchedulesViewercs.Designer.cs
+++ trunk/NEW TRUNK/CustomUserControl/DefenseSchedulesViewercs.Designer.cs
@@ -28,12 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DefenseSchedulesViewercs));
             this.labelDisplayMsg = new System.Windows.Forms.Label();
             this.dateTimePickerEnd = new System.Windows.Forms.DateTimePicker();
             this.label2 = new System.Windows.Forms.Label();
             this.dateTimePickerStart = new System.Windows.Forms.DateTimePicker();
             this.buttonViewDefScheds = new System.Windows.Forms.Button();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // labelDisplayMsg
@@ -46,7 +60,7 @@
             // 
             // dateTimePickerEnd
             // 
-            this.dateTimePickerEnd.Location = new System.Drawing.Point(185, 48);
+            this.dateTimePickerEnd.Location = new System.Drawing.Point(432, 9);
             this.dateTimePickerEnd.Name = "dateTimePickerEnd";
             this.dateTimePickerEnd.Size = new System.Drawing.Size(200, 20);
             this.dateTimePickerEnd.TabIndex = 10;
@@ -54,7 +68,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(276, 32);
+            this.label2.Location = new System.Drawing.Point(401, 15);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(16, 13);
             this.label2.TabIndex = 9;
@@ -66,10 +80,11 @@
             this.dateTimePickerStart.Name = "dateTimePickerStart";
             this.dateTimePickerStart.Size = new System.Drawing.Size(200, 20);
             this.dateTimePickerStart.TabIndex = 8;
+            this.dateTimePickerStart.ValueChanged += new System.EventHandler(this.dateTimePickerStart_ValueChanged);
             // 
             // buttonViewDefScheds
             // 
-            this.buttonViewDefScheds.Location = new System.Drawing.Point(3, 9);
+            this.buttonViewDefScheds.Location = new System.Drawing.Point(16, 8);
             this.buttonViewDefScheds.Name = "buttonViewDefScheds";
             this.buttonViewDefScheds.Size = new System.Drawing.Size(163, 26);
             this.buttonViewDefScheds.TabIndex = 7;
@@ -77,27 +92,111 @@
             this.buttonViewDefScheds.UseVisualStyleBackColor = true;
             this.buttonViewDefScheds.Click += new System.EventHandler(this.buttonViewDefScheds_Click);
             // 
-            // richTextBox1
+            // dataGridView1
             // 
-            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.richTextBox1.Location = new System.Drawing.Point(3, 97);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(932, 545);
-            this.richTextBox1.TabIndex = 6;
-            this.richTextBox1.Text = "";
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            dataGridViewCellStyle1.NullValue = null;
+            this.dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCellsExceptHeaders;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7});
+            this.dataGridView1.Location = new System.Drawing.Point(2, 74);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(934, 568);
+            this.dataGridView1.TabIndex = 12;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Course";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 60;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Title";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Width = 300;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Date";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Width = 70;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Time";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            this.Column4.Width = 90;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Venue";
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            this.Column5.Width = 70;
+            // 
+            // Column6
+            // 
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.Column6.DefaultCellStyle = dataGridViewCellStyle2;
+            this.Column6.HeaderText = "Adviser/s";
+            this.Column6.Name = "Column6";
+            this.Column6.ReadOnly = true;
+            this.Column6.Width = 150;
+            // 
+            // Column7
+            // 
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.Column7.DefaultCellStyle = dataGridViewCellStyle3;
+            this.Column7.HeaderText = "Panel/s";
+            this.Column7.Name = "Column7";
+            this.Column7.ReadOnly = true;
+            this.Column7.Width = 150;
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileName = "DefenseSchedules";
+            this.saveFileDialog1.Filter = resources.GetString("saveFileDialog1.Filter");
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(163, 23);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Save Defense Schedule";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // DefenseSchedulesViewercs
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.labelDisplayMsg);
             this.Controls.Add(this.dateTimePickerEnd);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.dateTimePickerStart);
             this.Controls.Add(this.buttonViewDefScheds);
-            this.Controls.Add(this.richTextBox1);
             this.Name = "DefenseSchedulesViewercs";
             this.Size = new System.Drawing.Size(940, 645);
+            this.Load += new System.EventHandler(this.DefenseSchedulesViewercs_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -110,6 +209,15 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.DateTimePicker dateTimePickerStart;
         private System.Windows.Forms.Button buttonViewDefScheds;
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button1;
     }
 }
--- trunk/NEW TRUNK/CustomUserControl/DefenseSchedulesViewercs.cs
+++ trunk/NEW TRUNK/CustomUserControl/DefenseSchedulesViewercs.cs
@@ -6,6 +6,7 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.IO;
 
 namespace CustomUserControl
 {
@@ -30,7 +31,6 @@ namespace CustomUserControl
             }
             else
             {
-                richTextBox1.Text = "";
                 labelDisplayMsg.Text = "Invalid range. Make sure the first date is before or the same as the second.";
             }
         }
@@ -40,47 +40,194 @@ namespace CustomUserControl
             String query;
             List<String>[] list;
             List<String>[] panelist;
+            int rowIndex;
 
-            richTextBox1.Text = "";
-            String text;
+            //Course, Title, Date, Time, Venue, Adviser, Panels
+            dataGridView1.Rows.Clear();
+            rowIndex = 0;
 
-            for (DateTime i = start; i.CompareTo(end) != 1; i = i.AddDays(1))
+            for (DateTime i = start; i.CompareTo(end) != 1; i = i.AddDays(1)) 
             {
-                text = "";
-                if (!i.DayOfWeek.ToString().Equals("Sunday"))
+                query = "select t.thesisgroupid, t.course, t.title, d.defensedatetime, d.place from defenseschedule d, thesisgroup t where d.thesisgroupid = t.thesisgroupid";
+                query += " and datepart(yyyy,d.defensedatetime) =" + i.Year + " and datepart(mm,d.defensedatetime) =" + i.Month + " and datepart(dd,d.defensedatetime) =" + i.Day + " order by t.course, t.section, d.defensedatetime;";
+
+                list = dbHandler.Select(query, 5);
+
+                for (int j = 0; j < list[0].Count(); j++, rowIndex++) 
                 {
-                    query = "select t.thesisgroupid, t.course, t.section, t.title, d.defensedatetime, d.place from defenseschedule d, thesisgroup t where d.thesisgroupid = t.thesisgroupid";
-                    query += " and datepart(yyyy,d.defensedatetime) =" + i.Year + " and datepart(mm,d.defensedatetime) =" + i.Month + " and datepart(dd,d.defensedatetime) =" + i.Day + " order by t.course, t.section, d.defensedatetime;";
+                    dataGridView1.Rows.Add();
+                    dataGridView1.Rows[rowIndex].Cells[0].Value = list[1].ElementAt(j);
+                    dataGridView1.Rows[rowIndex].Cells[1].Value = list[2].ElementAt(j);
 
-                    list = dbHandler.Select(query, 6);
+                    string dateTimeString = string.Format("{0:M/d/yyyy H:mm}", list[3].ElementAt(j));
 
-                    if (list[0].Count != 0)
+                    int month = Convert.ToInt16(dateTimeString.Split(' ')[0].Split('/')[0]);
+                    int day = Convert.ToInt16(dateTimeString.Split(' ')[0].Split('/')[1]);
+                    int year = Convert.ToInt16(dateTimeString.Split(' ')[0].Split('/')[2]);
+                    int hour = Convert.ToInt16(dateTimeString.Split(' ')[1].Split(':')[0]);
+                    int minute = Convert.ToInt16(dateTimeString.Split(' ')[1].Split(':')[1]);
+
+                    DateTime dateTime = new DateTime(year, month, day, hour, minute, 0);
+
+                    dataGridView1.Rows[rowIndex].Cells[2].Value = string.Format("{0:M/d/yyyy}",dateTime);
+
+                    if(list[1].ElementAt(j).Equals("THSST-1"))
+                        dataGridView1.Rows[rowIndex].Cells[3].Value = string.Format("{0:HHmm}", dateTime) + " - " + string.Format("{0:HHmm}", dateTime.AddHours(1));
+                    else
+                        dataGridView1.Rows[rowIndex].Cells[3].Value = string.Format("{0:HHmm}", dateTime) + " - " + string.Format("{0:HHmm}", dateTime.AddHours(2));
+
+                    dataGridView1.Rows[rowIndex].Cells[4].Value = list[4].ElementAt(j);
+
+                    // panelists
+                    query = "select p.lastName, p.firstName, p.mi from panelist p, panelassignment pa where pa.panelistid = p.panelistid and pa.thesisgroupid = " + list[0].ElementAt(j) + " order by p.lastname ;";
+                    panelist = dbHandler.Select(query, 3);
+
+                    String panelistCellText = "";
+                    for (int k = 0; k < panelist[0].Count(); k++) 
                     {
+                        panelistCellText += panelist[0].ElementAt(k) + ", " + panelist[1].ElementAt(k) + " " + panelist[2].ElementAt(k) + ".";
+                        if (k < panelist[0].Count() - 1)
+                            panelistCellText += "\n";
+                    } 
 
-                        text += i.Date + " (" + i.DayOfWeek + ")\n";
-                        for (int j = 0; j < list[0].Count; j++)
-                        {
-                            //Console.WriteLine(list[3].ElementAt(j));
-                            text += "\t" + list[3].ElementAt(j) + "\n\t";
-                            text += list[1].ElementAt(j) + " " + list[2].ElementAt(j) + "\n\t";
-                            text += list[4].ElementAt(j) + " " + list[5].ElementAt(j) + "\n\t\t";
-                            query = "select p.firstName, p.MI, p.lastName from panelist p, panelassignment pa where pa.panelistid = p.panelistid and pa.thesisgroupid = " + list[0].ElementAt(j) + ";";
-                            panelist = dbHandler.Select(query, 3);
-                            //Console.WriteLine(2);
-                            text += panelist[0].ElementAt(0) + " " + panelist[1].ElementAt(0) + ". " + panelist[2].ElementAt(0) + "\n\t\t";
-                            text += panelist[0].ElementAt(1) + " " + panelist[1].ElementAt(1) + ". " + panelist[2].ElementAt(1) + "\n\t\t";
-                            text += panelist[0].ElementAt(2) + " " + panelist[1].ElementAt(2) + ". " + panelist[2].ElementAt(2) + "\n";
-                        }
-                    }
-                    richTextBox1.Text += text;
+                    dataGridView1.Rows[rowIndex].Cells[6].Value = panelistCellText;
                 }
             }
 
-            if (richTextBox1.Text.Equals(""))
-                richTextBox1.Text = "There are no defense schedules within this range.";
+            if (dataGridView1.Rows.Count == 0)
+            {
+                MessageBox.Show("There are no defense schedules within the selected range", "Notice", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                button1.Enabled = false;
+            }
+            else
+                button1.Enabled = true;
+        }
+
+        private void dateTimePickerStart_ValueChanged(object sender, EventArgs e)
+        {
+            dateTimePickerEnd.Value = dateTimePickerStart.Value.AddDays(6);
+        }
+
+        private void DefenseSchedulesViewercs_Load(object sender, EventArgs e)
+        {
+            dateTimePickerStart.Value = DateTime.Now;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            //  FilterIndex
+            //  1 - CSV file - for Thesis Groups
+            //  2 - CSV file - for Panelists
+            //  3 - CSV file - general format
+            //  4 - TXT file - for Thesis Groups
+            //  5 - TXT file - for Panelists
+            //  6 - TXT file - general format
+            //
+            //  Formats:
+            //  for Thesis Groups:  includes title, date, time, venue
+            //  for Panelists:      includes title, date, time, venue, panel
+            //  general format:     includes all (course, title, date, time, venue, panel)
+
+            Boolean isCSVfile = false;
+            Boolean saveCourseColumn = false;
+            Boolean savePanelColumn = false;
+
+            if (saveFileDialog1.FilterIndex < 4)
+                isCSVfile = true;
+
+            switch (saveFileDialog1.FilterIndex) 
+            { 
+                case 2:
+                case 5: savePanelColumn = true;      
+                    break;
+
+                case 3:
+                case 6: savePanelColumn = true;      
+                        saveCourseColumn = true;     
+                    break;
+            }
+
+            string lineToWrite;
+
+            if (CheckIfFileIsBeingUsed(saveFileDialog1.FileName))
+                MessageBox.Show("File is currently in use", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else
+                using (StreamWriter sw = new StreamWriter(saveFileDialog1.FileName))
+                {
+                    for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                    {
+                        lineToWrite = "";
+
+                        if (saveCourseColumn)
+                        {
+                            lineToWrite += dataGridView1.Rows[i].Cells[0].Value;
+
+                            if (isCSVfile)
+                                lineToWrite += ",";
+                            else
+                                lineToWrite += " ";
+                        }
+
+                        // index column
+                        //   1    title
+                        //   2    date
+                        //   3    time
+                        //   4    venue
+                        for (int j = 1; j <= 4; j++)
+                        {
+                            lineToWrite += dataGridView1.Rows[i].Cells[j].Value;
+
+                            if (j < 4 || savePanelColumn)
+                                if (isCSVfile)
+                                    lineToWrite += ",";
+                                else
+                                    lineToWrite += " ";
+                        }
+
+                        if (savePanelColumn)
+                        {
+                            string[] panels = dataGridView1.Rows[i].Cells[6].Value.ToString().Split('\n');
+
+                            for (int j = 0; j < panels.Count(); j++)
+                            {
+                                lineToWrite += "\"" + panels[j] + "\"";
+
+                                if (j < panels.Count() - 1)
+                                    if (isCSVfile)
+                                        lineToWrite += ",";
+                                    else
+                                        lineToWrite += " ";
+                            }
+                        }
+                        sw.WriteLine(lineToWrite);
+                    }
+                    sw.Flush();
+                    sw.Close();
+                }
+        }
+        public bool CheckIfFileIsBeingUsed(string fileName)
+        {
+            FileStream fs;
+
+            try
+            {
+                fs = File.Open(fileName, FileMode.Open, FileAccess.Read, FileShare.None);
+            }
+            catch (System.IO.IOException exp)
+            {
+                return true;
+            }
+
+            fs.Close();
+            return false;
 
         }
 
-      
     }
 }
