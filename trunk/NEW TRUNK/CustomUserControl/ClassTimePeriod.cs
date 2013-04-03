dff03902ad79f6389e3b988fe4f545480aef74c6
lordd466b@gmail.com <lordd466b@gmail.com@8da0477a-1d21-c455-1106-59f872cc08d5>
2013-04-03 19:17:35 +0000

    added panelistID

--- trunk/NEW TRUNK/CustomUserControl/ClassTimePeriod.cs
+++ trunk/NEW TRUNK/CustomUserControl/ClassTimePeriod.cs
@@ -12,20 +12,23 @@ namespace CustomUserControl
         private String course;
         private String section;
         private String day;
+        private String panelist;
 
         //Not in the uml
         public String Course { get { return course; } }
         public String Section { get { return section; } }
         public String Day { get { return day; } }
+        public String Panelist { get { return panelist; } }
         //end
         
-        public ClassTimePeriod(int id, String section, String course, String day, DateTime startTime, DateTime endTime) 
+        public ClassTimePeriod(int id, String section, String course, String day, DateTime startTime, DateTime endTime, String panelistID) 
             :base(startTime, endTime)
         {
             this.id = id;
             this.section = section;
             this.course = course;
             this.day = day;
+            this.panelist = panelistID;
         }
 
         public override String ToString() 
